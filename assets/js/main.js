7ab10bafb57c07ee699561929b45c66d2c7bdab1
Nick Frost <nickfrost.us@gmail.com>
2021-05-09 00:48:25 -0400

    add service worker

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,17 @@ function checkLocalstorage() {
 }
 
 checkLocalstorage();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js', {scope: '/'})
+        .then(function (registration) {
+            console.log('Service Worker Registered');
+        });
+
+    navigator.serviceWorker
+        .ready
+        .then(function (registration) {
+            console.log('Service Worker Ready');
+        });
+}
